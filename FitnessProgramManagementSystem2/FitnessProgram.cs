4c2c470a45d677f765c8445f23d0ed337339e84d
Gowsi Anu <gowsig926@gmail.com>
2024-10-11 06:09:38 +0530

    Q1

---
+++ FitnessProgramManagementSystem2/FitnessProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem2
+{
+    internal class FitnessProgram
+    {
+        public int FitnessProgramId;
+        public string Title;
+        public string Duration;
+        public decimal Price;
+    }
+}
